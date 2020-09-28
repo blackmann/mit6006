3067b8e93532ea9f12985f95ed6cd97323dd75a8
De-Great Yartey <yarteydegreat2@gmail.com>
2020-09-28 15:50:05 +0000

    Solved, 1try, AC

---
+++ src/main/kotlin/codeforces/BeautifulMatrix.kt
@@ -0,0 +1,22 @@
+package codeforces
+
+import kotlin.math.abs
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+
+fun main() {
+    var y = 0
+    var x = -1
+
+    while (y < 5) {
+        x = readInts().indexOf(1)
+        if (x > -1) break
+
+        y++
+    }
+
+    println(abs(2 - x) + abs(2 - y))
+}
