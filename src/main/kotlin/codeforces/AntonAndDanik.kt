edf747e9df58583b97d4419ea1294ded346cabaf
De-Great Yartey <yarteydegreat2@gmail.com>
2020-09-28 15:22:01 +0000

    Solved anton and danik, 3 tries, AC

---
+++ src/main/kotlin/codeforces/AntonAndDanik.kt
@@ -0,0 +1,29 @@
+package codeforces
+
+// Problem statement: http://codeforces.com/contest/734/problem/A
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+
+fun main() {
+    var a = 0
+    var d = 0
+
+    readInt()
+
+    readLn().forEach {
+        if (it == 'A') a++
+        else d++
+    }
+
+    if (a == d) {
+        println("Friendship")
+    }
+    else if (a > d) {
+        println("Anton")
+    } else {
+        println("Danik")
+    }
+}
