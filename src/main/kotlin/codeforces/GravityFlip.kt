7738a254d17bcf81b941d5ad5e085740cda26872
De-Great Yartey <yarteydegreat2@gmail.com>
2020-09-29 17:33:24 +0000

    Solved, 2 tries, AC

---
+++ src/main/kotlin/codeforces/GravityFlip.kt
@@ -0,0 +1,18 @@
+package codeforces
+
+// Problem statement: http://codeforces.com/contest/405/problem/A
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+
+fun main() {
+    val s = readInt()
+
+    val h = readInts().toMutableList()
+
+    h.sort()
+
+    println(h.joinToString(" "))
+}
